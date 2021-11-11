4a2c0ef03b31987cf2cfe576dfd781cb38ec6b38
Kateryna-Borysenko <k.borysenko.kyiv@gmail.com>
2021-11-11 02:48:21 +0200

    Completed task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryConatainer = document.querySelector('.gallery');
+
+const imagesMarkup = createImagesMarkup(galleryItems);
+
+galleryConatainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+galleryConatainer.addEventListener('click', onImageContainerClick);
+
+function createImagesMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+                <a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>
+            `;
+        })
+        .join('');
+}
+
+
+function onImageContainerClick(e) {
+    e.preventDefault()
+    const isPreviewImage = e.target.classList.contains('gallery__image');
+    if (!isPreviewImage) {
+        return;
+    }
+    // настройки с библиотеки  SimpleLightbox
+    const lightbox = new SimpleLightbox('.gallery a', {
+    
+        captionsData: 'alt', //получить заголовок из данного атрибута
+        captionDelay: 250    //задержка перед отображением подписи
+        
+    });
+}
+
+// console.log(galleryItems);
